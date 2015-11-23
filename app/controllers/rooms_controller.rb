12fb1da1391ca18aef92301726d5680a84d4b7fe
Ron De Benedetti <RonDeBen@gmail.com>
2015-11-23 15:57:23 -0600

    rooms can now draw cards

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -6,4 +6,9 @@ class RoomsController < ApplicationController
 		@rooms = Room.all
 	end
 
+	def draw_card
+		room = Room.find_by_id(params[:id])
+		@card = room.deck.top_card
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers:{ registrations: 'registrations' }
 
-  get 'deck/:id/top_card' => 'decks#top_card'
+  get 'room/:id/top_card' => 'rooms#draw_card'
   get 'rooms' => 'rooms#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
